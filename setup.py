b2df1124121bf732fb2035de394fcd6fc809a381
Matthew R. Rupp <ruppmatt@gmail.com>
2017-01-25 17:10:18 -0500

    More movement of materials for packaging.

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup
 
 setup(
     name='AEDeveloper',
+    version='0.0.1',
+    description='AvidaED Developer Web Utilities',
+    author='Matthew Rupp',
+    author_email='ruppmatt@gmail.com',
     packages=['AEDeveloper'],
     include_package_data=True,
     install_requires=[
         'flask',
         'Pygments',
         'mysql-connector',
+        'flask-security',
+        'flask-cors',
     ],
 )
